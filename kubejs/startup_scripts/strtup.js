d294c19c77d03684fb6201334dd811cd338e0410
Cactusstudent <97865546+Cactusstudent@users.noreply.github.com>
2022-08-11 14:01:58 +0800

    Update kubejs/startup_scripts/strtup.js

--- kubejs/startup_scripts/strtup.js
+++ kubejs/startup_scripts/strtup.js
@@ -9,7 +9,7 @@ onEvent('item.registry', event => {
 	event.create('crypto_currency').displayName('加密货币')
 	event.create('encrypted_ingot').displayName('§5加密锭§r').glow(true)
 	event.create('encrypted_matter').displayName('加密物质')
-	event.create('finished_program').displayName('§3<§1已完成的代码§3/>_§r').glow(true).unstackable()
+	event.create('finished_program').displayName('§3<§1已完成的项目§3/>_§r').glow(true).unstackable()
 	event.create('creative_essence').displayName('创造精华').glow(true)
 	event.create('raw_andesite_alloy').displayName('§6无机 §r安山混合物')
 	event.create('wooden_gear').displayName('木齿轮')
