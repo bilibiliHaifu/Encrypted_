30e70aed9808dfb4fce99c6505b56eefc62f70cb
bilibiliHaifu <104065661+bilibiliHaifu@users.noreply.github.com>
2022-08-11 08:52:08 +0800

    kubejs物品汉化

--- kubejs/startup_scripts/strtup.js
+++ kubejs/startup_scripts/strtup.js
@@ -5,16 +5,16 @@ console.info('Custom [Items / Blocks]')
 onEvent('item.registry', event => {
 	
 	// Register new items here
-	event.create('new_compound_ethylene').displayName('Ethylene')
-	event.create('crypto_currency').displayName('Crypto Currency')
-	event.create('encrypted_ingot').displayName('§5Encrypted Ingot§r').glow(true)
-	event.create('encrypted_matter').displayName('Encrypted Matter')
-	event.create('finished_program').displayName('§3<§1Finished_program§3/>_§r').glow(true).unstackable()
-	event.create('creative_essence').displayName('Creative Essence').glow(true)
-	event.create('raw_andesite_alloy').displayName('§6Lifeless §rAndesite Mixture')
-	event.create('wooden_gear').displayName('Wooden Gear')
-	event.create('wooden_plate').displayName('Wooden Plate')
-	event.create('incomplete_machine_frame', 'create:sequenced_assembly').displayName('Incomplete Machine Frame').unstackable()
+	event.create('new_compound_ethylene').displayName('乙烯')
+	event.create('crypto_currency').displayName('加密货币')
+	event.create('encrypted_ingot').displayName('§5加密锭§r').glow(true)
+	event.create('encrypted_matter').displayName('加密物质')
+	event.create('finished_program').displayName('§3<§1已完成的代码§3/>_§r').glow(true).unstackable()
+	event.create('creative_essence').displayName('创造精华').glow(true)
+	event.create('raw_andesite_alloy').displayName('§6无机 §r安山混合物')
+	event.create('wooden_gear').displayName('木齿轮')
+	event.create('wooden_plate').displayName('木片')
+	event.create('incomplete_machine_frame', 'create:sequenced_assembly').displayName('未完成的机器框架').unstackable()
 })
 
 onEvent('block.registry', event => {
